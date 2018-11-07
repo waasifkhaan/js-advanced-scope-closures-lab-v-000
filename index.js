a88ca3e3d107dd8c73b5520bf508370f268705dc
Wasif Khan <waasif.khaan@gmail.com>
2018-11-07 02:51:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange){
   return function (x,y){
-    if (y-x > blockRange){
+    if ((y-x) > blockRange){
       return `${y-x-range} blocks out of range`
     }
     else {
